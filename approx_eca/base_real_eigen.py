abf58466b4522528c8ba5cc94d74260589a9f0f9
Chen Miao <rzchen2014@gmail.com>
2020-03-29 17:53:39 +0800

    updated approx ECA

---
+++ approx_eca/base_real_eigen.py
@@ -0,0 +1,117 @@
+from __future__ import print_function
+import os
+import shutil
+
+import numpy as np
+
+import tensorflow as tf
+import keras
+from keras import layers
+# from keras.datasets import mnist
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Flatten, Input
+from keras.layers import Conv2D, MaxPooling2D
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.utils import custom_object_scope
+from keras import backend as K
+
+import real_eigen as RealEigen
+from real_eigen import Projection, EigenDist, GATE_FACTOR, MAGIC_CODE, wrap_norm, raise_dim, get_time
+from load_data import load_data
+from save_history import save_history
+from other_models import lg, lda, qda, svm, compare_all, save_compare_result
+
+batch_size = 128
+epochs = 12
+
+# state_len = 784
+
+WORK_MAGIC_CODE = "PLAIN"
+
+data_tag = "mnist"
+
+# HP_ORTHONORMAL ~ 10 if initialized with orthogonaly
+# HP_EIGENDIST ~ 10 / (state_len * num_classes)
+#GATE_FACTOR = K.constant(100)
+#GATE_FACTOR = 50
+#RealEigen.GATE_FACTOR = GATE_FACTOR
+RealEigen.HP_ORTHONORMAL = 10
+RealEigen.HP_EIGENDIST = 0.001
+RealEigen.HP_LESSSENS = 1
+
+
+def build_model(state_len, num_classes):
+	print("Building model...")
+
+	model = Sequential()
+	model.add(Projection(state_len, input_shape=(1, state_len)))
+	model.add(EigenDist(num_classes, input_shape=(1, state_len)))
+	model.add(Flatten())
+	model.summary()
+
+	model.compile(loss=keras.losses.categorical_crossentropy,
+	#model.compile(loss=keras.losses.mean_squared_error,
+	              optimizer=keras.optimizers.Adadelta(),
+	              metrics=['accuracy'])
+
+	return model
+
+def eigen_train(data_tag, to_train=False, is_bin=False):
+	print("Unpacking data...")
+	state_len, num_classes, x_train, y_train, x_test, y_test = load_data(data_tag)
+	print(f"state_len: {state_len}, num_classes: {num_classes}")
+
+	model = build_model(state_len, num_classes)
+	print("Model built. ")
+
+	time_stamp = get_time()
+	print(time_stamp)
+	model_save_root = f"checkpoints/{data_tag}/{MAGIC_CODE}"
+	history_save_root = f"history/{data_tag}/{MAGIC_CODE}/{time_stamp}"
+	os.makedirs(model_save_root, exist_ok=True)
+	os.makedirs(history_save_root, exist_ok=True)
+
+	model_basename = f"{MAGIC_CODE}-{data_tag}-{WORK_MAGIC_CODE}"
+	model_save_path = f"{model_save_root}/{model_basename}-{time_stamp}.h5"
+	model_universal = f"best_models/{model_basename}.h5"
+	history = []
+	if to_train:
+		# earlystopper = EarlyStopping(patience=10, verbose=1, monitor="val_acc")
+		# checkpointer = ModelCheckpoint(model_universal, verbose=1, save_best_only=True, monitor="val_acc")
+
+		earlystopper = EarlyStopping(patience=10, verbose=1)
+		checkpointer = ModelCheckpoint(model_universal, verbose=1, save_best_only=True)
+
+		# reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, epsilon=1e-4, mode='min')
+		history = model.fit(x_train, y_train,
+		          batch_size=batch_size,
+		          epochs=epochs,
+		          verbose=1,
+		          validation_data=(x_test, y_test), 
+		          # callbacks=[earlystopper, checkpointer, reduce_lr_loss])
+		          callbacks=[earlystopper, checkpointer])
+
+	with custom_object_scope({"Projection":Projection, "EigenDist": EigenDist}):
+		model.load_weights(model_universal)
+		if to_train:
+			model.save(model_save_path)
+
+	score = model.evaluate(x_test, y_test, verbose=0)
+	print('Test loss:', score[0])
+	print('Test accuracy:', score[1])
+
+	dataset = [x_train, y_train, x_test, y_test]
+	save_history(dataset, model, num_classes, history, data_tag, WORK_MAGIC_CODE, MAGIC_CODE, history_save_root, time_stamp)
+	cmp_res = compare_all(dataset, model, data_tag, WORK_MAGIC_CODE, MAGIC_CODE, time_stamp, is_bin=is_bin)
+	save_compare_result(cmp_res, data_tag, WORK_MAGIC_CODE, MAGIC_CODE, time_stamp)
+
+	print("Wait Nutstore to sync...")
+	import time
+	time.sleep(5)
+
+	shutil.copy(f"history_{data_tag}.txt", f"{history_save_root}/")
+
+if __name__ == '__main__':
+	eigen_train(data_tag, to_train=False, is_bin=False)
+
+
---
+++ approx_eca/wis.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+import os
+import shutil
+
+import numpy as np
+
+import tensorflow as tf
+import keras
+from keras import layers
+# from keras.datasets import mnist
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Flatten, Input
+from keras.layers import Conv2D, MaxPooling2D
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.utils import custom_object_scope
+from keras import backend as K
+
+import real_eigen as RealEigen
+from real_eigen import Projection, EigenDist, GATE_FACTOR, MAGIC_CODE, wrap_norm, raise_dim, get_time
+from load_data import load_data
+from save_history import save_history
+from other_models import lg, lda, qda, svm, compare_all, save_compare_result
+import base_real_eigen as base_model
+from base_real_eigen import eigen_train
+
+batch_size = 128
+epochs = 120
+
+# state_len = 784
+
+WORK_MAGIC_CODE = "PLAIN"
+
+data_tag = "wis"
+
+# HP_ORTHONORMAL ~ 10 if initialized with orthogonaly
+# HP_EIGENDIST ~ 10 / (state_len * num_classes)
+#GATE_FACTOR = K.constant(100)
+#GATE_FACTOR = 50
+#RealEigen.GATE_FACTOR = GATE_FACTOR
+RealEigen.HP_ORTHONORMAL = 1
+RealEigen.HP_EIGENDIST = 0.5
+RealEigen.HP_LESSSENS = 1
+
+base_model.WORK_MAGIC_CODE = WORK_MAGIC_CODE
+base_model.batch_size = batch_size
+base_model.epochs = epochs
+
+# state_len, num_classes, x_train, y_train, x_test, y_test = load_data(data_tag)
+# dataset = x_train, y_train, x_test, y_test
+
+if __name__ == '__main__':
+	eigen_train(data_tag, to_train=False, is_bin=True)
+
+
